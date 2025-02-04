e459e35ec51be1ac70f06b554f8454bfedac5590
prosk67 <proyashsk@gmail.com>
2025-02-04 17:19:04 +0600

    Added some rough files

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+
+  <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+    <Link href="/"> Audio Store</Link>
+  </div>
+  )
 };
 
 export default Navbar;
